683de0c1f8cd908dc222b5fd9ec197c91ba09eee
annewithoutani <marieletin0146@gmail.com>
2025-07-04 20:04:09 -0300

    first commit

---
+++ ep2.asm
@@ -0,0 +1,17 @@
+#───────────────────────────────────────────╖╓──────────────────────────────#
+# 				Requisito 1					║║			Requisito 2			#
+#───────────────────────────────────────────╢╟──────────────────────────────#
+#	*float ordena(int tam, int tipo,		║║	Os dados devem ser			#
+# 	float *vetor), onde:					║║	lidos de um arquivo,		#
+# 											║║	o qual será passado			#
+# 	· ordena: Nome da função;				║║	como entrada para			#
+# 	· int tam: Tamanho do vetor a ser		║║	o programa.					#
+#	ordenado;								║║								#
+#	· int tipo: Especifica qual método		║║	O vetor ordenado			#
+# 	será utilizado;							║║	de forma crescente			#
+#	· float *vetor: Vetor com os valores	║║	deve ser escrito			#
+#	a serem ordenados;						║║	no final do arquivo.		#
+#	· retorno (*float): Retorna um			║║								#
+#	ponteiro para o vetor ordenado.			║║								#
+#───────────────────────────────────────────╜╙──────────────────────────────#
+
